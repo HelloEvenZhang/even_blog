f05bfb6462edbc8b70bb2bfaccdd1b00bb843873
even.z <383490032@qq.com>
2023-06-16 16:50:02 +0800

    init project.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,42 +6,4 @@ set :repo_url, "https://github.com/HelloEvenZhang/even_blog.git"
 set :branch, "release"
 
 append :linked_files, "config/database.yml", "config/master.key"
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads"
-
-# puma相关配置
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_conf, "#{shared_path}/puma.rb"
-
-set :puma_preload_app, true
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/release`
-        puts "WARNING: HEAD is not the same as origin/release"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-  
-  before :starting,     :check_revision
-end
+append :linked_dirs, "log", "storage", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads"
