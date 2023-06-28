b87fdc8899aef0549fb95e84b0ff1982cfd01f1c
even.z <383490032@qq.com>
2023-06-28 16:28:20 +0800

    init project.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  config.action_cable.allowed_request_origins = ['https://www.helloeven.com']
-  config.action_cable.url = "wss://www.helloeven.com/cable"
+  # config.action_cable.mount_path = nil
+  # config.action_cable.url = "wss://example.com/cable"
+  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
