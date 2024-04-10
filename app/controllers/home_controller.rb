5ff68f265047689103cb6f8f6a27eaf212c619f1
even.z <383490032@qq.com>
2024-04-10 16:50:37 +0800

    Add Home Style

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    @lastest_posts = Post.order(created_at: :desc).first(5)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
-  root "posts#index"
+  root "home#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts do
