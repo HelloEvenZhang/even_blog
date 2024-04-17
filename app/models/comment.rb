569689531d272ce628594132b9b1310c4a79a9d7
even.z <383490032@qq.com>
2024-04-17 16:56:51 +0800

    add default_scope

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   validates :name, length: { maximum: 30 }
-  validates :content, presence: true  
+  validates :content, presence: true
+
+  default_scope { order(:created_at) }
 end
