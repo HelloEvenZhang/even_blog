e35b6a2b5305c369050e15ebafc6f72a89e08e04
even.z <383490032@qq.com>
2024-04-24 16:28:23 +0800

    优化部署

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,4 +8,10 @@ set :branch, "release"
 append :linked_files, "config/database.yml", "config/master.key"
 append :linked_dirs, "log", "storage", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads"
 
-before 'deploy:assets:precompile', 'bundle exec rails tailwindcss:build'
+namespace :deploy do
+  namespace :assets do
+    before :precompile, :build_tailwindcss do
+      execute "$HOME/.rbenv/bin/rbenv exec bundle exec rails tailwindcss:build"
+    end
+  end
+end
